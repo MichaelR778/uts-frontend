01a1d0c0d7229eab53ebe87094b35a0202f3c43e
Michael Rahardja <michaelrahardja23@gmail.com>
2024-10-03 14:38:10 +0700

    product page ver 1

--- product_script.js
+++ product_script.js
@@ -19,6 +19,48 @@ const romance = [
 
 romance.forEach((manga) => $("#romance").append(createItem(manga)));
 
+const comedy = [
+  {
+    img: "https://cdn.myanimelist.net/r/334x484/images/manga/3/219741.webp?s=6b2efbdb0544ae26a9d43d5601863090",
+    title: "Spy x Family",
+  },
+  {
+    img: "https://cdn.myanimelist.net/images/anime/12/76049.webp",
+    title: "One Punch Man",
+  },
+  {
+    img: "https://cdn.myanimelist.net/r/334x484/images/manga/2/245008.webp?s=6e7b7c34b33409274d10a56b05eb2e11",
+    title: "Horimiya",
+  },
+  {
+    img: "https://cdn.myanimelist.net/images/anime/1895/142748.webp",
+    title: "Kono Subarashii Sekai ni Shukufuku wo!",
+  },
+];
+
+comedy.forEach((manga) => $("#comedy").append(createItem(manga)));
+
+const action = [
+  {
+    img: "https://cdn.myanimelist.net/r/334x484/images/manga/3/216464.webp?s=abbbeacac7f765052ebf5d03f869cf0b",
+    title: "Chainsaw Man",
+  },
+  {
+    img: "https://cdn.myanimelist.net/images/anime/12/76049.webp",
+    title: "One Punch Man",
+  },
+  {
+    img: "https://cdn.myanimelist.net/images/anime/1286/99889.webp",
+    title: "Kimetsu no Yaiba",
+  },
+  {
+    img: "https://cdn.myanimelist.net/images/anime/1614/90408.webp",
+    title: "Darling in the FranXX",
+  },
+];
+
+action.forEach((manga) => $("#action").append(createItem(manga)));
+
 function createItem(manga) {
   var item = $('<div class="item"></div>');
 
